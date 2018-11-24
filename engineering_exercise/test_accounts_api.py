47bbe5edbcfde2aba0091853dfd571e5023d490f
raoulsullivan <stephen@beyondspreadsheets.co.uk>
2018-11-24 14:39:53 +0200

    transaction endpoint

--- engineering_exercise/test_accounts_api.py
+++ engineering_exercise/test_accounts_api.py
@@ -1,15 +1,18 @@
 """ Tests the functionality concerning how engineering_exercise interacts with Accounts objects
 """
-
+import datetime
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
-from fintech.models import Account
+from fintech.models import Account, Transaction
 
+
+TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 
 class AccountViewsTestCase(TestCase):
     """ Tests for the Account API Views """
+
     def setUp(self):
         """ A small set of test data """
         self.superuser = User.objects.create_superuser(
@@ -18,7 +21,15 @@ class AccountViewsTestCase(TestCase):
         for i in range(10):
             user = User.objects.create_user(username='Test user {}'.format(i))
             for j in range(2):
-                Account.objects.create(user=user, name='Account {}'.format(j), balance=0)
+                account = Account.objects.create(user=user, name='Account {}'.format(j), balance=5)
+                for k in range(5):
+                    Transaction.objects.create(
+                        account=account,
+                        transaction_date=datetime.datetime.today().date(),
+                        amount=1,
+                        active=True,
+                        description='Transaction {}'.format(k),
+                    )
 
     @staticmethod
     def test_setup():
@@ -44,5 +55,26 @@ class AccountViewsTestCase(TestCase):
         response = self.client.get(url)
         assert response.json() == {
             'uuid': str(account.uuid),
-            'balance': '0.00',
+            'balance': '5.00',
         }
+
+    def test_transaction(self):
+        """ Should get a list of serialised transactions """
+        account = Account.objects.first()
+        url = reverse('account-transaction', args=(account.uuid,))
+        self.client.login(username=self.superuser.username, password='derp')
+        response = self.client.get(url)
+        assert response.status_code == 200
+        assert len(response.json()) == 5
+        transaction_1 = account.transactions.all()[0]
+        expected_response = {
+            'uuid': str(transaction_1.uuid),
+            'account': str(account.uuid),
+            'transaction_date': datetime.datetime.today().date().isoformat(),
+            'amount': '1.00',
+            'description': 'Transaction 0',
+            'active': True,
+            'create_time': transaction_1.create_time.strftime(TIMESTAMP_FORMAT),
+            'update_time': transaction_1.update_time.strftime(TIMESTAMP_FORMAT),
+        }
+        self.assertEqual(response.json()[0], expected_response)
--- engineering_exercise/views.py
+++ engineering_exercise/views.py
@@ -6,7 +6,7 @@ from rest_framework import serializers, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from fintech.models import Account
+from fintech.models import Account, Transaction
 
 
 class AccountSerializer(serializers.HyperlinkedModelSerializer):
@@ -23,6 +23,22 @@ class AccountBalanceSerializer(serializers.HyperlinkedModelSerializer):
         model = Account
         fields = ('uuid', 'balance')
 
+class TransactionSerializer(serializers.ModelSerializer):
+    """ Serialises the base information regarding a Transaction """
+    class Meta: #pylint: disable=R0903
+        """ All fields """
+        model = Transaction
+        fields = (
+            'uuid',
+            'account',
+            'transaction_date',
+            'amount',
+            'description',
+            'active',
+            'create_time',
+            'update_time'
+        )
+
 
 class AccountViewSet(viewsets.GenericViewSet): # pylint: disable=R0901
     """ Allows users to interact with the Account object"""
@@ -34,4 +50,23 @@ class AccountViewSet(viewsets.GenericViewSet): # pylint: disable=R0901
         """ Retrieves the balance"""
         account = self.get_object()
         serializer = AccountBalanceSerializer(account, many=False)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'])
+    def transaction(self, request, pk=None): #pylint: disable=C0103
+        """ Retrieves the transaction listing"""
+        account = self.get_object()
+
+        # Users may not interact with Transactions that are not 'active'
+        # TODO - behaviour here dependent on the auth framework.
+        # We currently assume only staff users have access
+        if request.user.is_staff:
+            query = Transaction.objects.filter(account=account)
+        else:
+            raise NotImplementedError()
+            #query = Transaction.objects.filter(account=account, active=True)
+
+        transactions = query.all()
+
+        serializer = TransactionSerializer(transactions, many=True)
         return Response(serializer.data)
