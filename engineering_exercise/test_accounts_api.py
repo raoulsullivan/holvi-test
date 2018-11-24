94c04d73d4283720dc04b8bc54246c07e671d2a6
raoulsullivan <stephen@beyondspreadsheets.co.uk>
2018-11-24 12:09:27 +0200

    test authentication

--- engineering_exercise/test_accounts_api.py
+++ engineering_exercise/test_accounts_api.py
@@ -1,13 +1,26 @@
-from django.test import TestCase
 from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
 
 from fintech.models import Account
 
 
 class AccountViewsTestCase(TestCase):
     def setUp(self):
+        self.superuser = User.objects.create_superuser(username='Superuser', is_staff=True, password='derp', email='stephen@saruste.fi')
         for i in range(10):
-            user = User.objects.create(username='Test user {}'.format(i))
+            user = User.objects.create_user(username='Test user {}'.format(i))
+            for j in range(2):
+                Account.objects.create(user=user, name='Account {}'.format(j), balance=0)
+
+    def test_setup(self):
+        assert User.objects.count() == 11
+        assert Account.objects.count() == 20
 
     def test_account_authentication(self):
-        assert User.objects.count() == 10
+        url = reverse('account-list')
+        response = self.client.get(url)
+        assert response.status_code == 403
+        self.client.login(username=self.superuser.username, password='derp')
+        response = self.client.get(url)
+        assert response.status_code == 200
