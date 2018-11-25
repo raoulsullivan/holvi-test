c464f4f75a50f1df2dda02f82c225cd433bbaa28
raoulsullivan <stephen@beyondspreadsheets.co.uk>
2018-11-25 18:03:09 +0200

    fixing bugs with validation on delete and update

--- fintech/admin.py
+++ fintech/admin.py
@@ -1,5 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.shortcuts import redirect
 from .models import Account, Transaction
+from .errors import AccountBalanceError
 
 class TransactionInline(admin.TabularInline):
     model = Transaction
@@ -27,6 +29,20 @@ class TransactionAdmin(admin.ModelAdmin):
     search_fields = ('description', 'account__name', 'account__user__username')
     list_filter = ['transaction_date']
     ordering = ('-transaction_date', '-create_time')
+
+    def save_model(self, request, obj, form, change):
+        try:
+            super().save_model(request, obj, form, change)
+        except AccountBalanceError as err:
+            messages.error(request, err)
+            return redirect('admin:fintech_transaction_changelist')
+
+    def delete_model(self, request, obj):
+        try:
+            super().delete_model(request, obj)
+        except AccountBalanceError as err:
+            messages.error(request, err)
+            return redirect('admin:fintech_transaction_changelist')
 
 admin.site.register(Account, AccountAdmin)
 admin.site.register(Transaction, TransactionAdmin)
