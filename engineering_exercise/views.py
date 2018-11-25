c39852dc967bb96bb3db4d479d52f974843660c3
raoulsullivan <stephen@beyondspreadsheets.co.uk>
2018-11-25 08:47:42 +0200

    fiddling with serialisers

--- engineering_exercise/views.py
+++ engineering_exercise/views.py
@@ -2,15 +2,22 @@
 
 Implementations of the Django REST framework pattern
 """
-from rest_framework import serializers, viewsets
+import copy
+from rest_framework import serializers, viewsets, status
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 from fintech.models import Account, Transaction
 
+class AccountBalanceValidationError(serializers.ValidationError):
+    pass
 
-class AccountSerializer(serializers.HyperlinkedModelSerializer):
+def account_balance_validator(value):
+    raise AccountBalanceValidationError('derp')
+
+
+class AccountSerializer(serializers.ModelSerializer):
     """ Serialises the base information regarding an Account """
     class Meta: #pylint: disable=R0903
         """ All fields """
@@ -37,8 +44,16 @@ class TransactionSerializer(serializers.ModelSerializer):
             'description',
             'active',
             'create_time',
-            'update_time'
+            'update_time',
         )
+        #validators = [account_balance_validator]
+        extra_kwargs = {
+            'uuid': {'read_only': True},
+            'account': {'read_only': True},
+            'active': {'read_only': True},
+            'create_time': {'read_only': True},
+            'update_time': {'read_only': True},
+        }
 
 
 class AccountViewSet(viewsets.GenericViewSet): # pylint: disable=R0901
@@ -55,8 +70,7 @@ class AccountViewSet(viewsets.GenericViewSet): # pylint: disable=R0901
         return Response(serializer.data)
 
 
-    @staticmethod
-    def _transactions_get(request, account):
+    def _transactions_get(self, request, account):
         """ Customers may not interact with Transactions that are not 'active'
         TODO - behaviour here dependent on the auth framework.
         We currently assume only staff users, not customers, have access
@@ -73,13 +87,23 @@ class AccountViewSet(viewsets.GenericViewSet): # pylint: disable=R0901
         paginator = PageNumberPagination()
         paginator.page_size = 10
         paginated_queryset = paginator.paginate_queryset(transactions, request)
-        serializer = TransactionSerializer(paginated_queryset, many=True)
+        serializer = self.serializer_class(paginated_queryset, many=True)
         return paginator.get_paginated_response(serializer.data)
 
 
-    @staticmethod
-    def _transactions_post(request, account):
-        raise NotImplementedError()
+    def _transactions_post(self, request, account):
+        """ Creates the transaction object and updates the account balance
+        TODO - move the update of account balance to the database model
+        """
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            transaction = Transaction(**serializer.validated_data)
+            transaction.active = True
+            transaction.account = account
+            transaction.save()
+            serializer = self.serializer_class(transaction, many=False)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
     @action(detail=True, methods=['get', 'post'])
@@ -87,8 +111,14 @@ class AccountViewSet(viewsets.GenericViewSet): # pylint: disable=R0901
         """ For interacting with Transactions on this Account
         GET - Retrieves the Transaction listing for the Account
             Ordered with most recently transacted first
-        POST - Creates a new Transaction on the Account
+        POST - Creates a new Transaction on the Account. Send:
+            {
+                'transaction_date': '%Y-%m-%d',
+                'amount': decimal, 15 digits, 2 decimal places,
+                'description': 20 characters,
+            }.
         """
+        self.serializer_class = TransactionSerializer
         account = self.get_object()
 
         if request.method == 'POST':
